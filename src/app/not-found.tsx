52a52c9a15f9f84221596fcf5c2462f4760f3378
richard <richard@provalidator.com>
2025-02-20 16:16:15 +0900

    next app

---
+++ src/app/not-found.tsx
@@ -0,0 +1,7 @@
+export default function NotFoundpage() {
+  return (
+    <main className="min-h-screen flex flex-col items-center justify-center bg-gray-50 p-4">
+      <h1 className="text-4xl font-bold text-gray-800 mb-4">404 Not Found</h1>
+    </main>
+  );
+}
